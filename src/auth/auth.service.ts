dafaa8d589abff9eb15ce95e1f5e0b0e7311a274
nchuy099 <nchihuy099@gmail.com>
2026-01-12 17:01:49 +0700

    feat(auth): add jwt-strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
+
+    async login(loginDto: LoginDto) {
+        const existingUser = await this.usersService.findByEmailOrUsername(
+            loginDto.identifier,
+            loginDto.identifier
+        );
+
+        if (!existingUser) {
+            throw new BadRequestException('Email/username or password is incorrect');
+        }
+
+
+    }
+}
